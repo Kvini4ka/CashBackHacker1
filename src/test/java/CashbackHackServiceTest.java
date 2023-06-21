efdf80d3166597c60de71ede132b98d5da1dbcaa
Kvini4ka <kvini4ka@mail.ru>
2023-06-21 18:25:45 +0300

    jUnit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import ru.netology.CashbackHackService;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldShowUnder1000() {
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowOver1000() {
+        int amount = 1700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowEqual1000() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
+}
